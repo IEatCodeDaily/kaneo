ae88e175697ce7316dbfc3789999e2ec915fd022
Samuel Else <samuelelse2@gmail.com>
2026-08-06 10:22:10 -0300

    feat(i18n): add Brazilian Portuguese (pt-BR) locale (cherry-picked from upstream)

--- i18n/resources.ts
+++ i18n/resources.ts
@@ -1,4 +1,18 @@
 import enUS from "./en-US.json";
+import esES from "./es-ES.json";
+import frFR from "./fr-FR.json";
+import hiIN from "./hi-IN.json";
+import idID from "./id-ID.json";
+import itIT from "./it-IT.json";
+import koKR from "./ko-KR.json";
+import mkMK from "./mk-MK.json";
+import nlNL from "./nl-NL.json";
+import ptBR from "./pt-BR.json";
+import ruRU from "./ru-RU.json";
+import trTR from "./tr-TR.json";
+import ukUA from "./uk-UA.json";
+import viVN from "./vi-VN.json";
+import zhCN from "./zh-CN.json";
 
 export const supportedLocales = [
   "mk-MK",
@@ -10,6 +24,7 @@ export const supportedLocales = [
   "fr-FR",
   "id-ID",
   "ko-KR",
+  "pt-BR",
   "ru-RU",
   "tr-TR",
   "uk-UA",
@@ -66,3 +81,22 @@ export async function loadLocaleBundle(
   const mod = await load();
   return mod.default as LocaleBundle;
 }
+export const resources = {
+  "mk-MK": mkMK,
+  "nl-NL": nlNL,
+  "en-US": enUS,
+  "de-DE": deDE,
+  "el-GR": elGR,
+  "fr-FR": frFR,
+  "hi-IN": hiIN,
+  "id-ID": idID,
+  "it-IT": itIT,
+  "es-ES": esES,
+  "ko-KR": koKR,
+  "pt-BR": ptBR,
+  "ru-RU": ruRU,
+  "tr-TR": trTR,
+  "uk-UA": ukUA,
+  "vi-VN": viVN,
+  "zh-CN": zhCN,
+} as const;
